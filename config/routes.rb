6db7705f775c0b2e1c7ad2f2fbaf219317ab6d15
fang duan <df1228@gmail.com>
2018-05-15 21:59:28 +0800

    FIX

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :chips
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
     resources :users
