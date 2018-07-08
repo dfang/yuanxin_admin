52f4d5acfc280fa6fcf017d1be56b9e04b5d6926
fang duan <df1228@gmail.com>
2018-07-08 09:23:30 +0800

    MISC

--- app/fields/collection_select_field.rb
+++ app/fields/collection_select_field.rb
@@ -5,14 +5,14 @@ class CollectionSelectField < Administrate::Field::Base
   def to_s
     # data
     m = Matrix[*collection]
-    p m
+    # p m
     texts = m.column(0).to_a
-    p "texts #{texts}"
+    # p "texts #{texts}"
     values = m.column(1).to_a
-    p values
+    # p values
     i = values.index(data)
-    p "data #{data}"
-    p "index #{i}"
+    # p "data #{data}"
+    # p "index #{i}"
     if i.present?
       texts[i]
     else
